c1c8c62073e4181de2e15ed6c9fe74b5c2bbe625
Николай Кожевников <nkozhevn2000@gmail.com>
2021-07-11 14:47:41 +0300

    Error messages added.

--- Models/Project.cs
+++ Models/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,11 +11,23 @@ namespace Task_Tracker.Models
         // Project information
         public static int id = 0;
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter the name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter the date")]
         public DateTime OpeningDate { get; set; }
+
+        [Required(ErrorMessage = "Enter the date")]
         public DateTime ClosingDate { get; set; }
+
+        [Required(ErrorMessage = "Enter the status")]
         public ProjectStatuses Status { get; set; }
+
+        [Required(ErrorMessage = "Enter the description")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Enter the priority")]
         public int Priority { get; set; }
 
         // Class Project constructor
--- Models/Task.cs
+++ Models/Task.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Task_Tracker.Models;
 using Project = Task_Tracker.Models.Project;
 using Task_Tracker.Controllers;
+using System.ComponentModel.DataAnnotations;
 
 namespace Task_Tracker.Models
 {
@@ -13,10 +14,20 @@ namespace Task_Tracker.Models
         // Task information
         public static int id = 0;
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter the name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter the name of the project")]
         public string Project { get; set; }
+
+        [Required(ErrorMessage = "Enter the status")]
         public TaskStatuses Status { get; set; }
+
+        [Required(ErrorMessage = "Enter the description")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Enter the bumber for priority")]
         public int Priority { get; set; }
         
         // Class Task constructor
